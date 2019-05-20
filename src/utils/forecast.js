c17de41c23d65f46bbce29db23cd14f1490a2930
w00tw00t111 <w00tw00t111@users.noreply.github.com>
2019-05-19 22:46:09 -0500

    Updated Forecast Data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,9 +8,9 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' ' + 'It is currently ' + body.currently.temperature + ' degrees out.' + ' There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' ' + 'It is currently ' + body.currently.temperature + ' degrees out.' + ' There is a ' + body.daily.data[0].precipProbability * 100 + '% chance of rain. Real feel temperature is: ' + body.currently.apparentTemperature + ' degrees')
         }
-        
+        console.log(body.currently)
     })    
 }
 
